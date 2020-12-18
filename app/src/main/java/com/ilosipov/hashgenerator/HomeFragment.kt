5ffbf59baa735a8c406d05c76a2c2e7c0e5797d3
Ilya Osipov <il.osipov.gm@gmail.com>
2020-12-18 21:43:52 +0300

    updating home fragment and home view

--- app/src/main/java/com/ilosipov/hashgenerator/HomeFragment.kt
+++ app/src/main/java/com/ilosipov/hashgenerator/HomeFragment.kt
@@ -1,59 +1,45 @@
 package com.ilosipov.hashgenerator
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.databinding.DataBindingUtil
+import com.ilosipov.hashgenerator.databinding.FragmentHomeBinding
 
 /**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Class HomeFragment
+ * @author Ilya Osipov (mailto:il.osipov.gm@gmail.com)
+ * @since 18.12.2020
+ * @version $Id$
  */
+
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "HomeFragment"
+    }
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        Log.i(TAG, "onCreateView: initialization HomeFragment.")
+        binding = DataBindingUtil.inflate(LayoutInflater.from(requireContext()),
+            R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true)
+
+        val hasAlgorithms = resources.getStringArray(R.array.hash_algorithms)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hasAlgorithms)
+
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
     }
 }
