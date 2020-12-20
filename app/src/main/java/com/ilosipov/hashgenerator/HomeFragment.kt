1a32863f6c4b65f90ad2c538296494d232c6684b
Ilya Osipov <il.osipov.gm@gmail.com>
2020-12-20 14:07:43 +0300

    added home fragment animation

--- app/src/main/java/com/ilosipov/hashgenerator/HomeFragment.kt
+++ app/src/main/java/com/ilosipov/hashgenerator/HomeFragment.kt
@@ -6,7 +6,11 @@ import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.ilosipov.hashgenerator.databinding.FragmentHomeBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Class HomeFragment
@@ -33,10 +37,45 @@ class HomeFragment : Fragment() {
 
         val hasAlgorithms = resources.getStringArray(R.array.hash_algorithms)
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hasAlgorithms)
-
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
 
+        binding.btnGenerator.setOnClickListener {
+            lifecycleScope.launch {
+                applyAnimations()
+                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSuccessFragment())
+            }
+        }
+
         return binding.root
+    }
+
+    private suspend fun applyAnimations() {
+        binding.apply {
+            titleHomeFragment.animate().alpha(0F).duration = 400L
+            btnGenerator.animate().alpha(0F).duration = 400L
+            textInputLayout.animate()
+                .alpha(0F)
+                .translationXBy(1200F)
+                .duration = 400L
+            editPersonName.animate()
+                .alpha(0F)
+                .translationXBy(-1200F)
+                .duration = 400L
+        }
+        delay(300)
+
+        binding.apply {
+            successBackground.animate().apply {
+                alpha(1F).duration = 600L
+//                rotationBy(720F).duration = 600L
+                scaleXBy(900F).duration = 800L
+                scaleYBy(900F).duration = 800L
+            }
+        }
+        delay(500)
+
+        binding.successImageView.animate().alpha(1F).duration = 1000L
+        delay(1500L)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
